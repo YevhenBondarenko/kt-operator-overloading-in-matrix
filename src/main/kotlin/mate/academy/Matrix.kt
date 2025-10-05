b72e7875c9aeab41dc9014f00668cd8a0e2f6cea
YevhenBondarenko <bondarenkojek@gmail.com>
2025-10-05 19:26:22 +0200

    implemented plus and minus operators for matrix

--- src/main/kotlin/mate/academy/Matrix.kt
+++ src/main/kotlin/mate/academy/Matrix.kt
@@ -8,7 +8,19 @@ class Matrix(private val rows: Int, private val cols: Int) {
         data[row][col] = value
     }
 
-    // TODO: implement overloading operators for matrix addition and subtraction
+    operator fun plus(matrix: Matrix) = operatorFunction(matrix) { a, b -> a + b }
+
+    operator fun minus(matrix: Matrix) = operatorFunction(matrix) { a, b -> a - b }
+
+    private fun operatorFunction(matrix: Matrix, operation: (a: Int, b: Int) -> Int): Matrix {
+        val result = Matrix(rows, cols)
+        for (row in 0 until rows) {
+            for (col in 0 until cols) {
+                result[row, col] = operation.invoke(this[row, col], matrix[row, col])
+            }
+        }
+        return result
+    }
 
     override fun toString(): String {
         return data.joinToString(separator = "\n") { row -> row.joinToString(" ") }
